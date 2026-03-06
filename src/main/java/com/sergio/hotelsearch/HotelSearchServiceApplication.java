25430a39adfffd2a40a76cbe66de2c9932753957
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-06 16:30:28 -0300

    commit inicial
    commit inicial

---
+++ src/main/java/com/sergio/hotelsearch/HotelSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.hotelsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSearchServiceApplication.class, args);
+	}
+
+}
