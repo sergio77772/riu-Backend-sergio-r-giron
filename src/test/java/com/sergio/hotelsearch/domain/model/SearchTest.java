220af0afa2757510f64a9fe554f9bae8eab1eae3
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-08 19:03:54 -0300

    add coverage

--- src/test/java/com/sergio/hotelsearch/domain/model/SearchTest.java
+++ src/test/java/com/sergio/hotelsearch/domain/model/SearchTest.java
@@ -9,31 +9,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SearchTest {
 
+    private static final LocalDate CHECK_IN = LocalDate.of(2025, 1, 10);
+    private static final LocalDate CHECK_OUT = LocalDate.of(2025, 1, 15);
+
     @Test
     void shouldCreateSearchSuccessfully() {
 
-        Search search = new Search(
-                "1",
-                "hotel1",
-                LocalDate.of(2025,1,10),
-                LocalDate.of(2025,1,15),
-                List.of(30,25)
-        );
+        Search search = new Search("1", "hotel1", CHECK_IN, CHECK_OUT, List.of(30, 25));
 
-        assertEquals("hotel1", search.hotelId());
+        assertAll(
+                () -> assertEquals("hotel1", search.hotelId()),
+                () -> assertEquals(CHECK_IN, search.checkIn()),
+                () -> assertEquals(CHECK_OUT, search.checkOut()),
+                () -> assertEquals(List.of(30, 25), search.ages()));
     }
 
     @Test
     void shouldThrowExceptionIfCheckInAfterCheckOut() {
 
-        assertThrows(IllegalArgumentException.class, () ->
-                new Search(
-                        "1",
-                        "hotel1",
-                        LocalDate.of(2025,1,20),
-                        LocalDate.of(2025,1,10),
-                        List.of(30)
-                )
-        );
+        assertThrows(IllegalArgumentException.class, () -> new Search("1", "hotel1",
+                LocalDate.of(2025, 1, 20),
+                LocalDate.of(2025, 1, 10),
+                List.of(30)));
+    }
+
+    @Test
+    void shouldThrowExceptionIfHotelIdIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Search("1", null, CHECK_IN, CHECK_OUT, List.of(30)));
+    }
+
+    @Test
+    void shouldThrowExceptionIfHotelIdIsBlank() {
+
+        assertThrows(IllegalArgumentException.class, () -> new Search("1", "   ", CHECK_IN, CHECK_OUT, List.of(30)));
     }
 }
