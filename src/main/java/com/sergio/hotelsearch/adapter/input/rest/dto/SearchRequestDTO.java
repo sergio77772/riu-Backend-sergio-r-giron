8dbfb5c96b86777e85824c45cea19a9450c04065
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-08 11:05:04 -0300

    add swagger

--- src/main/java/com/sergio/hotelsearch/adapter/input/rest/dto/SearchRequestDTO.java
+++ src/main/java/com/sergio/hotelsearch/adapter/input/rest/dto/SearchRequestDTO.java
@@ -1,6 +1,7 @@
 package com.sergio.hotelsearch.adapter.input.rest.dto;
 
 import com.sergio.hotelsearch.domain.model.Search;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
@@ -11,30 +12,23 @@ import java.util.UUID;
 
 public record SearchRequestDTO(
 
-        @NotBlank
-        String hotelId,
+                @NotBlank @Schema(example = "hotel-1234", description = "ID del Hotel") String hotelId,
 
-        @NotBlank
-        String checkIn,
+                @NotBlank @Schema(example = "25/12/2026", description = "Formato: dd/MM/yyyy") String checkIn,
 
-        @NotBlank
-        String checkOut,
+                @NotBlank @Schema(example = "31/12/2026", description = "Formato: dd/MM/yyyy") String checkOut,
 
-        @NotEmpty
-        List<Integer> ages
-) {
+                @NotEmpty @Schema(example = "[30, 28, 12]", description = "Edades de los huéspedes") List<Integer> ages) {
 
-    private static final DateTimeFormatter FORMATTER =
-            DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    public Search toDomain() {
+        public Search toDomain() {
 
-        return new Search(
-                UUID.randomUUID().toString(),
-                hotelId,
-                LocalDate.parse(checkIn, FORMATTER),
-                LocalDate.parse(checkOut, FORMATTER),
-                List.copyOf(ages)
-        );
-    }
+                return new Search(
+                                UUID.randomUUID().toString(),
+                                hotelId,
+                                LocalDate.parse(checkIn, FORMATTER),
+                                LocalDate.parse(checkOut, FORMATTER),
+                                List.copyOf(ages));
+        }
 }
