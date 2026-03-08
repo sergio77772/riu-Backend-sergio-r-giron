5a0ca3faa60f339018a404bba8bda6ae8a562714
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-08 10:59:08 -0300

    add swagger
    ..

--- src/main/java/com/sergio/hotelsearch/adapter/input/rest/SearchController.java
+++ src/main/java/com/sergio/hotelsearch/adapter/input/rest/SearchController.java
@@ -5,6 +5,9 @@ import com.sergio.hotelsearch.adapter.input.rest.dto.SearchResponseDTO;
 import com.sergio.hotelsearch.application.usecase.CreateSearchUseCase;
 import com.sergio.hotelsearch.application.usecase.CountSearchUseCase;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
 
 import org.slf4j.Logger;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/search")
+@Tag(name = "Search API", description = "Hotel search operations")
 public class SearchController {
 
     private static final Logger log = LoggerFactory.getLogger(SearchController.class);
@@ -27,6 +31,7 @@ public class SearchController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a hotel search request")
     public SearchResponseDTO search(@Valid @RequestBody SearchRequestDTO request) {
 
         log.info("Received search request for hotelId={}", request.hotelId());
@@ -39,6 +44,7 @@ public class SearchController {
     }
 
     @GetMapping("/count")
+    @Operation(summary = "Get number of searches for a given searchId")
     public long count(@RequestParam String searchId) {
 
         log.info("Counting searches for searchId={}", searchId);
