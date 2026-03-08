73ab57777dc282fe3e8571c2d3115e8dbf2409a2
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-08 10:50:03 -0300

    ..
    ...

--- src/main/java/com/sergio/hotelsearch/adapter/output/persistence/repository/JpaSearchRepository.java
+++ src/main/java/com/sergio/hotelsearch/adapter/output/persistence/repository/JpaSearchRepository.java
@@ -2,6 +2,7 @@ package com.sergio.hotelsearch.adapter.output.persistence.repository;
 
 import com.sergio.hotelsearch.adapter.output.persistence.entity.SearchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -10,10 +11,10 @@ public interface JpaSearchRepository extends JpaRepository<SearchEntity, String>
 
     Optional<SearchEntity> findBySearchId(String searchId);
 
+    @Query("SELECT COUNT(s) FROM SearchEntity s WHERE s.hotelId = :hotelId AND s.checkIn = :checkIn AND s.checkOut = :checkOut AND s.ages = :ages")
     long countByHotelIdAndCheckInAndCheckOutAndAges(
             String hotelId,
             LocalDate checkIn,
             LocalDate checkOut,
-            String ages
-    );
+            String ages);
 }
