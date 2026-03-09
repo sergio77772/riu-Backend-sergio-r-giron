f15398daed8911ee63e4e96c74afe38904e9dee3
Sergio Ruben Giron <sergio_77772@hotmail.com>
2026-03-08 22:29:32 -0300

    Create GlobalExceptionHandler.java
    add handler

---
+++ src/main/java/com/sergio/hotelsearch/adapter/input/rest/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.sergio.hotelsearch.adapter.input.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+/**
+ * Global exception handler for REST layer.
+ * Translates domain and application exceptions into proper HTTP responses.
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgument(IllegalArgumentException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+}
